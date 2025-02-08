21b9c550e866b520c7feafea15c25552de2d7177
allydevper <alamaswilmer@gmail.com>
2025-02-08 15:27:46 -0500

    update

---
+++ src/constants/imagesPath.ts
@@ -0,0 +1,331 @@
+import type { ImageModel } from "../models/ImageModel";
+
+export const BigTechImages: ImageModel[] = [
+    {
+        src: "/src/assets/technologies/big/C-(CSharp).png",
+        alt: "C Sharp",
+        title: "C Sharp",
+    },
+    {
+        src: "/src/assets/technologies/big/Java.png",
+        alt: "Java",
+        title: "Java",
+    },
+    {
+        src: "/src/assets/technologies/big/JavaScript.png",
+        alt: "JavaScript",
+        title: "JavaScript",
+    },
+    {
+        src: "/src/assets/technologies/big/TypeScript.png",
+        alt: "TypeScript",
+        title: "TypeScript",
+    },
+    {
+        src: "/src/assets/technologies/big/PHP.png",
+        alt: "PHP",
+        title: "PHP",
+    },
+    {
+        src: "/src/assets/technologies/big/Python.png",
+        alt: "Python",
+        title: "Python",
+    },
+];
+
+export const NormalTechImages_r1: ImageModel[] = [
+    {
+        src: "/src/assets/technologies/normal/.NET-core.png",
+        alt: ".NET Core",
+        title: ".NET Core",
+    },
+    {
+        src: "/src/assets/technologies/normal/.NET-Framework.png",
+        alt: ".NET Framework",
+        title: ".NET Framework",
+    },
+    {
+        src: "/src/assets/technologies/normal/.NET-MAUI.png",
+        alt: ".NET MAUI",
+        title: ".NET MAUI",
+    },
+    {
+        src: "/src/assets/technologies/normal/Astro.png",
+        alt: "Astro",
+        title: "Astro",
+    },
+    {
+        src: "/src/assets/technologies/normal/Angular.png",
+        alt: "Angular",
+        title: "Angular",
+    },
+    {
+        src: "/src/assets/technologies/normal/Django.png",
+        alt: "Django",
+        title: "Django",
+    },
+    {
+        src: "/src/assets/technologies/normal/FastAPI.png",
+        alt: "FastAPI",
+        title: "FastAPI",
+    },
+    {
+        src: "/src/assets/technologies/normal/Gatsby.png",
+        alt: "Gatsby",
+        title: "Gatsby",
+    },
+    {
+        src: "/src/assets/technologies/normal/jQuery.png",
+        alt: "jQuery",
+        title: "jQuery",
+    },
+    {
+        src: "/src/assets/technologies/normal/Spring.png",
+        alt: "Spring",
+        title: "Spring",
+    },
+];
+
+export const NormalTechImages_r2: ImageModel[] = [
+    {
+        src: "/src/assets/technologies/normal/MySQL.png",
+        alt: "MySQL",
+        title: "MySQL",
+    },
+    {
+        src: "/src/assets/technologies/normal/Nest.js.png",
+        alt: "Nest.js",
+        title: "Nest.js",
+    },
+    {
+        src: "/src/assets/technologies/normal/Next.js.png",
+        alt: "Next.js",
+        title: "Next.js",
+    },
+    {
+        src: "/src/assets/technologies/normal/Node.js.png",
+        alt: "Node.js",
+        title: "Node.js",
+    },
+    {
+        src: "/src/assets/technologies/normal/Nuxt-JS.png",
+        alt: "Nuxt-JS",
+        title: "Nuxt-JS",
+    },
+    {
+        src: "/src/assets/technologies/normal/PostgresSQL.png",
+        alt: "PostgresSQL",
+        title: "PostgresSQL",
+    },
+    {
+        src: "/src/assets/technologies/normal/React.png",
+        alt: "React",
+        title: "React",
+    },
+    {
+        src: "/src/assets/technologies/normal/Redux.png",
+        alt: "Redux",
+        title: "Redux",
+    },
+    {
+        src: "/src/assets/technologies/normal/Tailwind-CSS.png",
+        alt: "Tailwind CSS",
+        title: "Tailwind CSS",
+    },
+    {
+        src: "/src/assets/technologies/normal/Visual-Studio.png",
+        alt: "Visual Studio",
+        title: "Visual Studio",
+    },
+];
+
+export const NormalTechImages_r3: ImageModel[] = [
+    {
+        src: "/src/assets/technologies/normal/Vite.js.png",
+        alt: "Vite.js",
+        title: "Vite.js",
+    },
+    {
+        src: "/src/assets/technologies/normal/Vue.js.png",
+        alt: "Vue.js",
+        title: "Vue.js",
+    },
+    {
+        src: "/src/assets/technologies/normal/Xamarin.png",
+        alt: "Xamarin",
+        title: "Xamarin",
+    },
+    {
+        src: "/src/assets/technologies/normal/Laravel.png",
+        alt: "Laravel",
+        title: "Laravel",
+    },
+    {
+        src: "/src/assets/technologies/normal/Docker.png",
+        alt: "Docker",
+        title: "Docker",
+    },
+    {
+        src: "/src/assets/technologies/normal/CSS3.png",
+        alt: "CSS3",
+        title: "CSS3",
+    },
+    {
+        src: "/src/assets/technologies/normal/HTML5.png",
+        alt: "HTML5",
+        title: "HTML5",
+    },
+    {
+        src: "/src/assets/technologies/normal/Bootstrap.png",
+        alt: "Bootstrap",
+        title: "Bootstrap",
+    },
+];
+
+export const SmallTechImagesBase: ImageModel[] = [
+    {
+        src: "/src/assets/technologies/small/AdonisJS.png",
+        alt: "AdonisJS",
+        title: "AdonisJS",
+    },
+    {
+        src: "/src/assets/technologies/small/Alpine.js.png",
+        alt: "Alpine.js",
+        title: "Alpine.js",
+    },
+    {
+        src: "/src/assets/technologies/small/Apache-Maven.png",
+        alt: "Apache Maven",
+        title: "Apache Maven",
+    },
+    {
+        src: "/src/assets/technologies/small/Appwrite.png",
+        alt: "Appwrite",
+        title: "Appwrite",
+    },
+    {
+        src: "/src/assets/technologies/small/Backbone.js.png",
+        alt: "Backbone.js",
+        title: "Backbone.js",
+    },
+    {
+        src: "/src/assets/technologies/small/Bash.png",
+        alt: "Bash",
+        title: "Bash",
+    },
+    {
+        src: "/src/assets/technologies/small/CakePHP.png",
+        alt: "CakePHP",
+        title: "CakePHP",
+    },
+    {
+        src: "/src/assets/technologies/small/Eclipse-IDE.png",
+        alt: "Eclipse-IDE",
+        title: "Eclipse-IDE",
+    },
+    {
+        src: "/src/assets/technologies/small/Electron.png",
+        alt: "Electron",
+        title: "Electron",
+    },
+    {
+        src: "/src/assets/technologies/small/Firebase.png",
+        alt: "Firebase",
+        title: "Firebase",
+    },
+    {
+        src: "/src/assets/technologies/small/Flutter.png",
+        alt: "Flutter",
+        title: "Flutter",
+    },
+    {
+        src: "/src/assets/technologies/small/GitHub.png",
+        alt: "GitHub",
+        title: "GitHub",
+    },
+    {
+        src: "/src/assets/technologies/small/Go.png",
+        alt: "Go",
+        title: "Go",
+    },
+    {
+        src: "/src/assets/technologies/small/Grafana.png",
+        alt: "Grafana",
+        title: "Grafana",
+    },
+    {
+        src: "/src/assets/technologies/small/Handlebars.png",
+        alt: "Handlebars",
+        title: "Handlebars",
+    },
+    {
+        src: "/src/assets/technologies/small/Insomnia.png",
+        alt: "Insomnia",
+        title: "Insomnia",
+    },
+    {
+        src: "/src/assets/technologies/small/Ionic.png",
+        alt: "Ionic",
+        title: "Ionic",
+    },
+    {
+        src: "/src/assets/technologies/small/JSON.png",
+        alt: "JSON",
+        title: "JSON",
+    },
+    {
+        src: "/src/assets/technologies/small/Kotlin.png",
+        alt: "Kotlin",
+        title: "Kotlin",
+    },
+    {
+        src: "/src/assets/technologies/small/Krakenjs.png",
+        alt: "Krakenjs",
+        title: "Krakenjs",
+    },
+    {
+        src: "/src/assets/technologies/small/Postman.png",
+        alt: "Postman",
+        title: "Postman",
+    },
+    {
+        src: "/src/assets/technologies/small/Yii-Framework.png",
+        alt: "Yii Framework",
+        title: "Yii Framework",
+    },
+    {
+        src: "/src/assets/technologies/small/Sass.png",
+        alt: "Sass",
+        title: "Sass",
+    },
+    {
+        src: "/src/assets/technologies/small/Solid.js.png",
+        alt: "Solid.js",
+        title: "Solid.js",
+    },
+    {
+        src: "/src/assets/technologies/small/SQL-Developer.png",
+        alt: "SQL Developer",
+        title: "SQL Developer",
+    },
+    {
+        src: "/src/assets/technologies/small/SQLite.png",
+        alt: "SQLite",
+        title: "SQLite",
+    },
+    {
+        src: "/src/assets/technologies/small/Swagger.png",
+        alt: "Swagger",
+        title: "Swagger",
+    },
+    {
+        src: "/src/assets/technologies/small/Three.js.png",
+        alt: "Three.js",
+        title: "Three.js",
+    },
+    {
+        src: "/src/assets/technologies/small/Ubuntu.png",
+        alt: "Ubuntu",
+        title: "Ubuntu",
+    },
+];
