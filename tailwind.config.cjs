fa6760947267bc4ce43f97510971bdc3bae0facd
allydevper <alamaswilmer@gmail.com>
2025-02-10 21:50:16 -0500

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,8 @@
 module.exports = {
-    darkMode: 'class', // Usa 'class' para controlar el tema oscuro
-    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
