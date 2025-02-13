0fb5d00b5128ee5e426c54d285c35cc2b0ff93df
allydevper <alamaswilmer@gmail.com>
2025-02-12 23:37:52 -0500

    Update ProjectTable.tsx

--- src/components/admin/ProjectTable.tsx
+++ src/components/admin/ProjectTable.tsx
@@ -7,9 +7,6 @@ interface ProjectTableProps {
 }
 
 const ProjectTable: React.FC<ProjectTableProps> = ({ projects, loading }) => {
-    if (loading) {
-        return <div>Loading...</div>;
-    }
 
     return (
         <div className="bg-gray-800 dark:bg-gray-800 shadow-xl rounded-lg lg:col-span-2">
@@ -23,26 +20,35 @@ const ProjectTable: React.FC<ProjectTableProps> = ({ projects, loading }) => {
                 </div>
             </div>
             <div className="px-6 py-6 overflow-x-auto h-120">
-                <table id="projectTable" className="min-w-full table-auto">
-                    <thead className="bg-gray-600 dark:bg-gray-700 border-b border-gray-600 dark:border-gray-700">
-                        <tr className="text-gray-300 dark:text-gray-300">
-                            <th className="py-3 px-4 font-semibold text-left">NOMBRE</th>
-                            <th className="py-3 px-4 font-semibold text-left">URL</th>
-                            <th className="py-3 px-4 font-semibold text-left">TECNOLOGÍAS</th>
-                            <th className="py-3 px-4 font-semibold text-left">DESCRIPCIÓN</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {projects.map((project, index) => (
-                            <tr key={index} className="hover:bg-gray-700 dark:hover:bg-gray-700 border-b border-gray-600 dark:border-gray-700">
-                                <td className="py-4 px-4 font-medium text-white dark:text-white">{project.name}</td>
-                                <td className="py-4 px-4"><a href={project.url} className="text-pink-400 dark:text-pink-300 hover:underline">{project.url}</a></td>
-                                <td className="py-4 px-4 text-gray-400 dark:text-gray-400">{project.technologies.join(', ')}</td>
-                                <td className="py-4 px-4 text-gray-400 dark:text-gray-400">{project.description}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                {
+                    (loading) ? <div>Cargando...</div> :
+                        <table id="projectTable" className="min-w-full table-auto">
+                            <thead className="bg-gray-600 dark:bg-gray-700 border-b border-gray-600 dark:border-gray-700">
+                                <tr className="text-gray-300 dark:text-gray-300">
+                                    <th className="py-3 px-4 font-semibold text-left">NOMBRE</th>
+                                    <th className="py-3 px-4 font-semibold text-left">URL</th>
+                                    <th className="py-3 px-4 font-semibold text-left">TECNOLOGÍAS</th>
+                                    <th className="py-3 px-4 font-semibold text-left">DESCRIPCIÓN</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {projects.length > 0 ? (
+                                    projects.map((project, index) => (
+                                        <tr key={index} className="hover:bg-gray-700 dark:hover:bg-gray-700 border-b border-gray-600 dark:border-gray-700">
+                                            <td className="py-4 px-4 font-medium text-white dark:text-white">{project.name}</td>
+                                            <td className="py-4 px-4"><a href={project.url} className="text-pink-400 dark:text-pink-300 hover:underline">{project.url}</a></td>
+                                            <td className="py-4 px-4 text-gray-400 dark:text-gray-400">{project.technologies.join(', ')}</td>
+                                            <td className="py-4 px-4 text-gray-400 dark:text-gray-400">{project.description}</td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan={4} className="py-4 px-4 text-center text-gray-400 dark:text-gray-400">No projects available</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                }
             </div>
         </div>
     );
