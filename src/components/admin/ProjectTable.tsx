b64bcfd2efecd989d76d39dfc04194ec57cc00b8
allydevper <alamaswilmer@gmail.com>
2025-02-13 21:55:51 -0500

    Update ProjectTable.tsx

--- src/components/admin/ProjectTable.tsx
+++ src/components/admin/ProjectTable.tsx
@@ -27,11 +27,11 @@ const ProjectTable: React.FC<ProjectTableProps> = ({ projects, loading, handleDe
                             <thead className="bg-gray-600 dark:bg-gray-700 border-b border-gray-600 dark:border-gray-700">
                                 <tr className="text-gray-300 dark:text-gray-300">
                                     <th className="px-2 font-semibold text-left"></th>
-                                    <th className="px-2 font-semibold text-left">NOMBRE</th>
+                                    <th className="px-2 font-semibold text-left w-24">NOMBRE</th>
                                     <th className="px-2 font-semibold text-left">REPO</th>
                                     <th className="px-2 font-semibold text-left">DEMO</th>
-                                    <th className="px-2 font-semibold text-left">TECNOLOGÍAS</th>
-                                    <th className="px-2 font-semibold text-left">DESCRIPCIÓN</th>
+                                    <th className="px-2 font-semibold text-left w-24">TECNOLOGÍAS</th>
+                                    <th className="px-2 font-semibold text-left w-96">DESCRIPCIÓN</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -43,7 +43,7 @@ const ProjectTable: React.FC<ProjectTableProps> = ({ projects, loading, handleDe
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" /><path d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z" /></svg>
                                                 </button>
                                             </td>
-                                            <td className="py-4 px-4 font-medium text-white dark:text-white">{project.name}</td>
+                                            <td className="py-4 px-4 font-medium text-white dark:text-white w-24">{project.name}</td>
                                             <td className="py-4 px-4">
                                                 <a href={project.url_project} target='_blank' className="cursor-pointer text-gray-400 hover:text-white flex items-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-brand-github">
@@ -60,8 +60,8 @@ const ProjectTable: React.FC<ProjectTableProps> = ({ projects, loading, handleDe
                                                         </a>
                                                 }
                                             </td>
-                                            <td className="py-4 px-4 text-gray-400 dark:text-gray-400">{project.technologies.join(', ')}</td>
-                                            <td className="py-4 px-4 text-gray-400 dark:text-gray-400">{project.description}</td>
+                                            <td className="py-4 px-4 text-gray-400 dark:text-gray-400 w-24">{project.technologies.join(', ')}</td>
+                                            <td className="py-4 px-4 text-gray-400 dark:text-gray-400 w-96">{project.description}</td>
                                         </tr>
                                     ))
                                 ) : (
