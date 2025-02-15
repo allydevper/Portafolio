ee2fcca762d9e61176d3f2404af040a47772ecbf
allydevper <alamaswilmer@gmail.com>
2025-02-15 14:50:04 -0500

    update form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,90 @@
+import { showToast } from '@/lib/customToast';
+import React, { useState } from 'react';
+import { Toaster } from 'sonner';
+
+const ContactForm: React.FC = () => {
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault();
+        setIsSubmitting(true);
+
+        const formDataObj = new FormData(e.currentTarget);
+        try {
+            const response = await fetch("https://formsubmit.co/ajax/alamaswilmer@gmail.com", {
+                method: "POST",
+                body: formDataObj,
+                headers: {
+                    Accept: "application/json",
+                },
+            });
+
+            if (response.ok) {
+                setFormData({ name: '', email: '', message: '' });
+                showToast("Mensaje enviado con éxito!", 'success');
+            } else {
+                showToast("Hubo un error al enviar el mensaje.", 'danger');
+            }
+        } catch (error) {
+            showToast("Hubo un error de conexión.", 'danger');
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <div>
+            <Toaster />
+            <h2 className="text-3xl font-bold mb-4 text-yellow-300">Contáctame</h2>
+            <p className="mb-4">
+                No dudes en comunicarte por correo electrónico <span className="font-bold">(alamaswilmer@gmail.com)</span> o usar el formulario de contacto a continuación.
+            </p>
+            <form id="contact-form" className="max-w-lg mx-auto" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Tu Nombre"
+                    className="w-full mb-4 p-2 rounded-lg bg-white text-black"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Tu Correo Electrónico"
+                    className="w-full mb-4 p-2 rounded-lg bg-white text-black"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+                <textarea
+                    name="message"
+                    placeholder="Tu Mensaje"
+                    className="w-full mb-4 p-2 rounded-lg bg-white text-black"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                ></textarea>
+                <button
+                    type="submit"
+                    id="submit-button"
+                    className="w-full py-3 bg-yellow-300 text-black font-bold rounded-full hover:bg-white hover:text-black transition duration-300 cursor-pointer"
+                    aria-label="Enviar mensaje"
+                    disabled={isSubmitting}
+                >
+                    {isSubmitting ? 'Enviando...' : 'Enviar'}
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default ContactForm;
