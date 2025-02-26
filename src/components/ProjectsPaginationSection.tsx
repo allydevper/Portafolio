fc50cc2241778e441538278a46511bcedc4ee376
allydevper <alamaswilmer@gmail.com>
2025-02-25 21:57:20 -0500

    Update ProjectsPaginationSection.tsx

--- src/components/ProjectsPaginationSection.tsx
+++ src/components/ProjectsPaginationSection.tsx
@@ -92,16 +92,6 @@ const ProjectsPaginationSection = () => {
                                         })}
                                     </div>
                                     <div className="ml-auto flex space-x-2">
-                                        {project.url_demo && (
-                                            <a
-                                                href={project.url_demo}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                className="px-4 py-2 bg-yellow-300 text-black font-bold rounded-md transition duration-300 cursor-pointer"
-                                            >
-                                                Demo
-                                            </a>
-                                        )}
                                         <a
                                             href={project.url_project}
                                             target="_blank"
@@ -112,8 +102,17 @@ const ProjectsPaginationSection = () => {
                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                                 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                                             </svg>
-                                            <span>Código</span>
                                         </a>
+                                        {project.url_demo && (
+                                            <a
+                                                href={project.url_demo}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="px-4 py-2 bg-black text-white font-bold rounded-md transition duration-300 cursor-pointer"
+                                            >
+                                                Demo
+                                            </a>
+                                        )}
                                     </div>
                                 </div>
                             </div>
